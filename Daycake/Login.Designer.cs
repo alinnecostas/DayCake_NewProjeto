274eb6a88df1dab95d08d330d6a80ced07551b6c
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-04-16 22:24:54 -0300

    Adicionar arquivos de projeto.

---
+++ Daycake/Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Daycake
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnAcessar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(117, 53);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(100, 50);
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(142, 128);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(33, 13);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Login";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLogin.Location = new System.Drawing.Point(117, 144);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(117, 206);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(142, 190);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(38, 13);
+            this.lblSenha.TabIndex = 3;
+            this.lblSenha.Text = "Senha";
+            // 
+            // btnAcessar
+            // 
+            this.btnAcessar.Location = new System.Drawing.Point(128, 261);
+            this.btnAcessar.Name = "btnAcessar";
+            this.btnAcessar.Size = new System.Drawing.Size(75, 23);
+            this.btnAcessar.TabIndex = 5;
+            this.btnAcessar.Text = "Acessar";
+            this.btnAcessar.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 380);
+            this.Controls.Add(this.btnAcessar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.picLogo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login de acesso";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnAcessar;
+    }
+}
+
---
+++ Daycake/Menu.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Daycake
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnsCadastroCliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsAgendamentos = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsRecebimentos = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsConsultaClientes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsConsultaAgendamentos = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsStatus = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnsCadastroCliente,
+            this.mnsAgendamentos,
+            this.mnsConsultaAgendamentos,
+            this.mnsConsultaClientes,
+            this.mnsRecebimentos,
+            this.mnsStatus,
+            this.mnsSair});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnsCadastroCliente
+            // 
+            this.mnsCadastroCliente.Name = "mnsCadastroCliente";
+            this.mnsCadastroCliente.Size = new System.Drawing.Size(106, 20);
+            this.mnsCadastroCliente.Text = "Cadastro Cliente";
+            // 
+            // mnsAgendamentos
+            // 
+            this.mnsAgendamentos.Name = "mnsAgendamentos";
+            this.mnsAgendamentos.Size = new System.Drawing.Size(100, 20);
+            this.mnsAgendamentos.Text = "Agendamentos";
+            // 
+            // mnsRecebimentos
+            // 
+            this.mnsRecebimentos.Name = "mnsRecebimentos";
+            this.mnsRecebimentos.Size = new System.Drawing.Size(94, 20);
+            this.mnsRecebimentos.Text = "Recebimentos";
+            // 
+            // mnsConsultaClientes
+            // 
+            this.mnsConsultaClientes.Name = "mnsConsultaClientes";
+            this.mnsConsultaClientes.Size = new System.Drawing.Size(111, 20);
+            this.mnsConsultaClientes.Text = "Consulta Clientes";
+            // 
+            // mnsConsultaAgendamentos
+            // 
+            this.mnsConsultaAgendamentos.Name = "mnsConsultaAgendamentos";
+            this.mnsConsultaAgendamentos.Size = new System.Drawing.Size(150, 20);
+            this.mnsConsultaAgendamentos.Text = "Consulta Agendamentos";
+            // 
+            // mnsStatus
+            // 
+            this.mnsStatus.Name = "mnsStatus";
+            this.mnsStatus.Size = new System.Drawing.Size(51, 20);
+            this.mnsStatus.Text = "Status";
+            // 
+            // mnsSair
+            // 
+            this.mnsSair.Name = "mnsSair";
+            this.mnsSair.Size = new System.Drawing.Size(38, 20);
+            this.mnsSair.Text = "Sair";
+            // 
+            // picImage
+            // 
+            this.picImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImage.Location = new System.Drawing.Point(0, 24);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(800, 426);
+            this.picImage.TabIndex = 1;
+            this.picImage.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnsCadastroCliente;
+        private System.Windows.Forms.ToolStripMenuItem mnsAgendamentos;
+        private System.Windows.Forms.ToolStripMenuItem mnsRecebimentos;
+        private System.Windows.Forms.ToolStripMenuItem mnsConsultaClientes;
+        private System.Windows.Forms.ToolStripMenuItem mnsConsultaAgendamentos;
+        private System.Windows.Forms.ToolStripMenuItem mnsStatus;
+        private System.Windows.Forms.ToolStripMenuItem mnsSair;
+        private System.Windows.Forms.PictureBox picImage;
+    }
+}
